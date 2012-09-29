9b48a506ff6e13d3106d57a32aace1e13698caa1
Adrian Papari <blackrax@gmail.com>
2012-09-29 02:06:55 +0200

    Single-file demo.

--- keyflection-demo/src/net/onedaybeard/keyflection/demo/DemoUI.java
+++ keyflection-demo/src/net/onedaybeard/keyflection/demo/DemoUI.java
@@ -1,9 +1,23 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS.libgdx file.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package net.onedaybeard.keyflection.demo;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
@@ -32,18 +46,6 @@ import net.onedaybeard.keyflection.KeyflectionInputListener;
 import net.onedaybeard.keyflection.KeyflectionInputProcessor;
 import net.onedaybeard.keyflection.Shortcut;
 
-/*******************************************************************************
- * Copyright 2011 See AUTHORS.libgdx file.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
 
 public class DemoUI implements ApplicationListener
 {
@@ -71,6 +73,17 @@ public class DemoUI implements ApplicationListener
 	private boolean drawHud = true;
 	private StageKeys stageActions;
 
+	public static void main(String[] args)
+	{
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "keyflection-demo";
+		cfg.useGL20 = false;
+		cfg.width = 1024;
+		cfg.height = 768;
+
+		new LwjglApplication(new DemoUI(), cfg);
+	}
+	
 	@Override
 	public void create()
 	{
